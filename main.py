a98fe0ad5d8dca2845d669143258b2bdbf10bee2
Adonis SIMO <simoadonis@yahoo.fr>
2021-10-24 15:20:26 +0100

    Add CSV saving file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
 
@@ -15,11 +16,23 @@ if response.status_code == 200:
         results.append({
             'title': block.find('h2', class_='headline').text,
             'description': block.find('p').text,
-            'count': block.find('div', class_='digg-count').text
+            'count': block.find('div', class_='digg-count').text,
+            'link': block.find('a', class_='block relative')['href']
         })
     # print("There is ",len(results), " Of articles onthe frontpage of https://digg.com/")
+    _file = open("digg.com.csv", "w")
+    writer = csv.writer(_file)
+    writer.writerow(["title", 'description', 'count', 'link' ])
     for r in results:
-        print(r)
+        # print(r)
+        writer.writerow([
+            r['title'],
+            r['description'],
+            r['count'],
+            r['link']
+        ])
+    _file.close()
+    print("The CSV is created !!!")
 
         
     # print(post_blocks, " >>>>> the len here" )
